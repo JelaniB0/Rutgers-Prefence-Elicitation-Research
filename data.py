53ad8b0cc50674187c41ca8f81da40099e312c1b
JelaniB0 <jb2154@scarletmail.rutgers.edu>
2026-01-06 19:17:37 -0500

    Added some minor notes to both python files that can be thought about for now and further talked about on friday, repo structure is bound to change but this is just for starters.

---
+++ data.py
@@ -0,0 +1,15 @@
+"""
+Some initial ideas on what data, resources, and tools could be used for CS course preference elicitation research:
+1. Publicly available datasets on student preferences, course evaluations, etc.
+2. University course catalogs and syllabi.
+3. Course Navigator data from Rutgers (possibly).
+4. Surveys or interviews with students about their course preferences.
+5. Academic performance data (grades, completion rates, transcripts) if accessible.
+6. Course level data (difficulty of course, course level, prereqs, student credit standing).
+
+Things to be done: 
+1. Identify and gather relevant datasets (though small for course preferences).
+2. Potentially create data sets through surveys or interviews.
+3. Find ways to access/utilize course catalog, navigator, and academic factors. 
+"""
+
